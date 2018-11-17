7d0204a19229ccbc0d0838abec61b811cb655007
Vincent Tran <vtran037@ucr.edu>
2018-11-16 18:38:56 -0800

    gmapping, rplidar, kobuki

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,22 @@
+# CMake generated Testfile for 
+# Source directory: /home/leonardo/catkin_ws/src
+# Build directory: /home/leonardo/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(kobuki-devel/kobuki)
+subdirs(kobuki-devel/kobuki_capabilities)
+subdirs(kobuki-devel/kobuki_rapps)
+subdirs(slam_gmapping-hydro-devel/slam_gmapping)
+subdirs(kobuki-devel/kobuki_keyop)
+subdirs(kobuki-devel/kobuki_auto_docking)
+subdirs(kobuki-devel/kobuki_bumper2pc)
+subdirs(rplidar_ros-master)
+subdirs(slam_gmapping-hydro-devel/gmapping)
+subdirs(kobuki-devel/kobuki_description)
+subdirs(kobuki-devel/kobuki_controller_tutorial)
+subdirs(kobuki-devel/kobuki_random_walker)
+subdirs(kobuki-devel/kobuki_safety_controller)
+subdirs(kobuki-devel/kobuki_node)
+subdirs(kobuki-devel/kobuki_testsuite)
---
+++ catkin_ws/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+export TURTLEBOT_3D_SENSOR="asus_xtion_pro"
+export TURTLEBOT_BASE="kobuki"
+export TURTLEBOT_BATTERY="/sys/class/power_supply/BAT0"
+export TURTLEBOT_INTERACTIONS_LIST="[turtlebot_bringup/admin.interactions, turtlebot_bringup/documentation.interactions, turtlebot_bringup/pairing.interactions, turtlebot_bringup/visualisation.interactions]"
+export TURTLEBOT_NAME="turtlebot"
+export TURTLEBOT_RAPP_PACKAGE_BLACKLIST="[]"
+export TURTLEBOT_RAPP_PACKAGE_WHITELIST="[rocon_apps, turtlebot_rapps]"
+export TURTLEBOT_SERIAL_PORT="/dev/kobuki"
+export TURTLEBOT_SIMULATION="false"
+export TURTLEBOT_STACKS="hexagons"
+export TURTLEBOT_TYPE="turtlebot"
+
+# modified environment variables
+export PKG_CONFIG_PATH="/home/leonardo/catkin_ws/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
+export PWD="/home/leonardo/catkin_ws/build"
+export PYTHONPATH="/home/leonardo/catkin_ws/devel/lib/python2.7/dist-packages:$PYTHONPATH"
